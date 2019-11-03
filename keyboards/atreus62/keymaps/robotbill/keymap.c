8153eed764e2103053c69772f4e5c67f03a96613
Joe Lewis <robotbill@gmail.com>
2019-11-03 08:23:31 -0900

    use different unicode arrows

--- keyboards/atreus62/keymaps/robotbill/keymap.c
+++ keyboards/atreus62/keymaps/robotbill/keymap.c
@@ -7,9 +7,6 @@
 // The underscores don't mean anything - you can have a layer called STUFF or any other name.
 // Layer names don't all need to be of the same length, obviously, and you can also skip them
 // entirely and just use numbers.
-//#define _DEFAULT 0
-//#define _NAV 1
-//#define _RESET 2
 #define _QWERTY 0
 #define _LOWER 1
 #define _RAISE 2
@@ -76,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ├───────┼───────┼───────┼───────┼───────┼───────┤                  ├───────┼───────┼───────┼───────┼───────┼───────┤
  * │       │       │ REWIND│  PLAY │FORWARD│       │                  │       │       │   {   │   }   │   [   │   ]   │
  * ├───────┼───────┼───────┼───────┼───────┼───────┤                  ├───────┼───────┼───────┼───────┼───────┼───────┤
- * │       │       │VOLUME⬆︎│  MUTE │VOLUME⬇︎│       │                  │ LEFT  │ DOWN  │  UP   │ RIGHT │       │       │
+ * │       │       │VOLUME↑│  MUTE │VOLUME↓│       │                  │ LEFT  │ DOWN  │  UP   │ RIGHT │       │       │
  * ├───────┼───────┼───────┼───────┼───────┼───────┤                  ├───────┼───────┼───────┼───────┼───────┼───────┤
  * │       │       │       │       │       │       │                  │       │       │   <   │   >   │       │       │
  * ├───────┼───────┼───────┼───────┼───────┼───────┼───────┐  ┌───────┼───────┼───────┼───────┼───────┼───────┼───────┤
